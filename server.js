075efb33cd3f1730b422dfd4460b24eeebe8f963
Maulik Pansuriya <maulikpansuriya36@gmail.com>
2022-12-22 11:02:13 +0100

    Diffrent user can chat with each other

--- server.js
+++ server.js
@@ -3,10 +3,6 @@ const http = require("http");
 const express = require("express");
 const socketio = require("socket.io");
 const formatMessage = require("./utils/messages");
-const createAdapter = require("@socket.io/redis-adapter").createAdapter;
-const redis = require("redis");
-require("dotenv").config();
-const { createClient } = redis;
 const {
   userJoin,
   getCurrentUser,
@@ -22,13 +18,6 @@ const io = socketio(server);
 app.use(express.static(path.join(__dirname, "public")));
 
 const botName = "DevChatties Bot";
-
-(async () => {
-  pubClient = createClient({ url: "redis://127.0.0.1:6379" });
-  await pubClient.connect();
-  subClient = pubClient.duplicate();
-  io.adapter(createAdapter(pubClient, subClient));
-})();
 
 // Run when client connects
 io.on("connection", (socket) => {
